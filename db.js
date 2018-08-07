0fd1dc5c5c51e3b3ee23d69e74a2fec3a3dc0cd5
shibinragh <shibin@toobler.com>
2018-08-07 18:49:22 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,4 @@
+module.exports = {
+	//'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	'url' : 'mongodb://shibinragh1:password123@ds147391.mlab.com:47391/ask-questions'
+}
